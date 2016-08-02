66b460f9c4f7cb7c565680d45c7220c7a4033cfe
David Yang <daworld.ny@gmail.com>
2016-08-02 21:58:29 +0500

    Tweak for module manager

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,6 +4,7 @@ return array(
  'route' => 'Team:Admin:Member@gridAction',
  'icon' => 'fa fa-user-secret fa-5x',
  'name' => 'Team',
+ 'caption' => 'Team',
  'order' => 5,
  'description' => 'Team module allows to manage your personal'
 );
