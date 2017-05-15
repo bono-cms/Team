17404e5e876815a989f539b356922b52f8592b02
David Yang <daworld.ny@gmail.com>
2017-05-15 18:49:13 +0500

    longtext instead of text

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE `bono_module_team` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`lang_id` INT NOT NULL,
 	`name` varchar(255) NOT NULL COMMENT "Member's name",
-	`description` TEXT NOT NULL COMMENT "Member description or biography",
+	`description` LONGTEXT NOT NULL COMMENT "Member description or biography",
 	`photo` varchar(255) NOT NULL,
 	`published` varchar(1) NOT NULL,
 	`order` INT NOT NULL COMMENT "Sort order"
