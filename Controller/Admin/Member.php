8fd01c8cd4b18b282c25ea4e62ccd56f3bf4d8f3
dave-yang <daworld.ny@gmail.com>
2018-12-21 13:52:21 +0500

    Use Grid component to generate table

--- Controller/Admin/Member.php
+++ Controller/Admin/Member.php
@@ -114,8 +114,8 @@ final class Member extends AbstractController
         $service = $this->getModuleService('teamManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
