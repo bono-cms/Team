9198b91dee0f41f9d54cadeb81d43fb3182d6905
dave-yang <daworld.ny@gmail.com>
2019-03-08 20:13:03 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'route' => 'Team:Admin:Member@gridAction',
- 'name' => 'Team',
- 'caption' => 'Team',
- 'order' => 5,
- 'description' => 'Team module allows to manage your personal'
+    'name' => 'Team',
+    'description' => 'Team module allows to manage your personal',
+    'menu' => array(
+        'name' => 'Team',
+        'icon' => 'fas fa-users',
+        'items' => array(
+            array(
+                'route' => 'Team:Admin:Member@gridAction',
+                'name' => 'View all members'
+            ),
+            array(
+                'route' => 'Team:Admin:Member@addAction',
+                'name' => 'Add a member'
+            ),
+            array(
+                'route' => 'Team:Admin:Config@indexAction',
+                'name' => 'Configuration'
+            )
+        )
+    )
 );
